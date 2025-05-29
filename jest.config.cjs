d4154988ff66ec73ee04ec76e2f52fb656ad1635
colaone1 <samconnor2020@outlook.com>
2025-05-29 14:32:52 +0100

    chore: switch to next/jest and remove custom Babel config for SWC compatibility

--- jest.config.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
-  moduleNameMapper: {
-    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-    '^@/(.*)$': '<rootDir>/$1',
-  },
-  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
-}; 
